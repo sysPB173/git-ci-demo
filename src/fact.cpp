277e2d615946c6d756b97d0c805f93b68f121aba
sysPB173 <sys1@post.com>
2019-02-25 20:24:42 +0100

    product size increased - longlong

--- src/fact.cpp
+++ src/fact.cpp
@@ -1,7 +1,7 @@
 #include "fact.h"
 
 int MyMath::factorial(int n) {
-    long int product = 1;
+    long long int product = 1;
     if (n < 0) return 0;
     for (int i = n; i > 0; i--) {
         product *= i;
